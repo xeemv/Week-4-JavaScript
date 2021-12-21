02bf7eab776861cf05176a7d162766e960f51fbe
xeemv <xeeyang08@gmail.com>
2021-12-20 18:55:40 -0600

    starting ES6: Template Literals section

--- wk4.js
+++ wk4.js
@@ -1,3 +1,5 @@
+// ES6: Const and Let section --- video 1
+
 var x = 10;
 { 
     let x = 2;
@@ -27,7 +29,7 @@ if we used the keyword var
 { 
    var x = 2;
 }
-constant = we cannot change the value of a variable once it is set this is useful for when we have values that we want to give a name and then never change that value  */
+constant = we cannot change the value of a variable once it is set this is useful for when we have values that we want to give a name and then never change that value*/
 
 const pi = 3.14159
 
@@ -40,3 +42,6 @@ console.log(pi);
 ----> "Uncaught SyntaxError: unterminated comment.... wk4.js:43 (this is the error line on our js file)"
 This is because const does not allow us to change the value (reference line 30). */
 
+
+
+//ES6: Template Literals
