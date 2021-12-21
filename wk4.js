c00c14a466fdf9013add6e2f370009af1de157f6
xeemv <xeeyang08@gmail.com>
2021-12-20 18:52:40 -0600

    end of ES6: Const and Let section

--- wk4.js
+++ wk4.js
@@ -3,7 +3,7 @@ var x = 10;
     let x = 2;
 }
 /* from line 2 - 4, this is considered a block. 
-Out side of this section, the x is still equal to 10
+Outside of this section, the x is still equal to 10
 the 2 is only in existance inside the block
 let = allows us to say w/in this block, x is equal to 2 
 this does not impact the x outside of that block
@@ -27,4 +27,16 @@ if we used the keyword var
 { 
    var x = 2;
 }
-constant = we cannot change the value of a variable once it is set this is useful for when we have */
+constant = we cannot change the value of a variable once it is set this is useful for when we have values that we want to give a name and then never change that value  */
+
+const pi = 3.14159
+
+console.log(pi);
+
+pi = 6; //this is where we were trying to change the variable
+
+console.log(pi);
+/* this section from line 36 - line 38 will result in an error on the console.log:
+----> "Uncaught SyntaxError: unterminated comment.... wk4.js:43 (this is the error line on our js file)"
+This is because const does not allow us to change the value (reference line 30). */
+
